74830a5b8d183fb815427f95e86aa4d9d2b98f86
amIsachin <amIsachin@hotmail.com>
2023-07-15 23:41:23 +0530

    init application class libraary and define genenric repository

---
+++ Application/Repositories/Classes/Repository.cs
@@ -0,0 +1,26 @@
+﻿using Application.Repositories.Interfaces;
+
+namespace Application.Repositories.Classes;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    public Task<bool> Delete(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<T>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> Update(T entity)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Application/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Repositories.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    Task<List<T>> GetAll();
+
+    Task<T> GetById(int id);
+
+    Task<bool> Update(T entity);
+
+    Task<bool> Delete(T entity);
+}
